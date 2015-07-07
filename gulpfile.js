446e7900c4b2b983a0a46c20c18753bfa50e235e
Nick Kuhn <nickuhn@gmail.comn>
2015-07-07 16:57:03 -0700

    initialized npm and created gulp file with dependencies.
    gulp runs tests using the 'gulp test' command, 'gulp lint' lints all js files in the main directory and test dir. 'gulp' does both

---
+++ gulpfile.js
@@ -0,0 +1,16 @@
+var gulp = require('gulp');
+var mocha = require('gulp-mocha');
+var jshint = require('gulp-jshint');
+
+gulp.task(('default'), ['test', 'lint'], function() {});
+
+gulp.task('test', function() {
+  return gulp.src('test/*.js')
+             .pipe(mocha( { reporter: 'nyan' } ));
+});
+
+gulp.task('lint', function(){
+  return gulp.src(['./test/*.js', '*.js'])
+              .pipe(jshint())
+              .pipe(jshint.reporter('jshint-stylish'));
+});
